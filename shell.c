f23d76943637ffa20a71d8fd5d203298cc869680
premiereelementllc <premiereelementllc@gmail.com>
2022-03-29 21:29:15 -0500

    base 3

--- shell.c
+++ shell.c
@@ -1,67 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <string.h>
+#include "shell.h"
 
-void read_command ( char cmd[], char *par[] )
+/**
+ * shell - infinite loop that runs the shell
+ * @ac: Arg count
+ * @av: args passed to shell at begining of prog
+ * @env: environment
+ * Return: void
+ */
+
+void shell(int ac, char **av, char **env)
 {
-	char line[1024];
-	int count = 0, i = 0, j = 0;
-	char *array[100], *pch;
-	
-	// read a single line
-	
-	for ( ;; ) {
-		int c = fgetc ( stdin );
-		line[count++] = (char) c;
-		if (c == '\n' ) break;
-	}
-	if ( count == 1 ) return;
-	pch = strtok ( line, "\n" );
+	char *tmp = NULL;
+	char *line;
+	char **args;
+	char *er;
+	char filename;
+	int flow;
+	int status = 1;
 
-	//parsing line into words
-	while ( pch != NULL ) {
-		array[i++] = strdup ( pch );
-		pch = strtok ( NULL, "\n");
-	}
-	//first word is command
-	strcpy ( cmd, array[0] );
-	//parameters
-	for ( int j = 0; j < i; j++ )
-		par[j] = array[j];
-	par[i] = NULL; //terminate the pram list
-}
+	er = "ERROR";
+	do {
+		prompt();
+		line = _getline();
+		args = split_line(line);
+		flow = bridge(args[0], args);
+		if (flow == 2)
 
-
-void type_prompt()
-{
-	static int first_time = 1;
-	if ( first_time ) {
-		const char* CLEAR_SCREEN_ANSI = "\e [1;1H\e[2J";
-		write(STDOUT_FILENO, CLEAR_SCREEN_ANSI,12);
-		first_time = 0;
-	}
-	printf ("#"); //the cursor
-}
-
-int main ()
-{
-	char cmd[100], command[100], *parameters[20];
-	
-	char *envp[] = { (char *) "PATH=/bin", 0 };
-	while ( 1 ) {
-		type_prompt();
-		read_command ( command, parameters );
-		if ( fork() != 0 )
-			wait (NULL);
-		else{
-		strcpy ( cmd, "/bin/" );
-		strcat ( cmd, command );
-		execve ( cmd, parameters, envp ); //execute command
+		{
+			filename = args[0]; 
+			args[0] = find_path(args[0], tmp, er);
+			if (args[0] == er)
+			{
+				args[0] = search_cwd(filename, er);
+				if (args[0] == filename)
+					write(1, er, 5);
+			}
 		}
-		if ( strcmp ( command, "exit" ) == 0 )
-			break;
+		if (args[0] != er)
+			status = execute_prog(args, line, env, flow);
+		free(line);
+		free(args);
 	}
-	return (0);
+	while (status);
+	if (!ac)
+		(void)ac;
+	if (!av)
+		(void)av;
+	if (!env)
+		(void)env;
 }

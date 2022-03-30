8e2d5420483561a04463d3d5f8bb06f55d950998
premiereelementllc <premiereelementllc@gmail.com>
2022-03-30 11:02:44 -0500

    deletion

--- shell.c
+++ shell.c
@@ -10,14 +10,6 @@
 
 void shell(int ac, char **av, char **env)
 {
-	static int first_time = 1;
-	if ( first_time ) {
-		const char* CLEAR_SCREEN_ANSI = "\e [1;1H\e[2J";
-		write(STDOUT_FILENO, CLEAR_SCREEN_ANSI,12);
-		first_time = 0;
-	
-	printf("#");
-	}
 
 	char *tmp = NULL;
 	char *line;
